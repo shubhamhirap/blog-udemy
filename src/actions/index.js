64ba6986ed8d2ce93f24a59290705db57ea25ff0
shubham hirap <shirap07@gmail.com>
2020-12-16 15:23:39 +0530

    complete app with optimization

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,38 @@
-import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from "lodash";
+import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => async dispatch => {
-  const response = await jsonPlaceholder.get('/posts');
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
 
-  dispatch({ type: 'FETCH_POSTS', payload: response.data });
+  // for better understanding of below code
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
 };
+
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
+
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
+
+// This is a solution to reduce numbers of requests per userId
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+// //  private function
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
